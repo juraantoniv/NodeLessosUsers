efe45edd837e7f9f947cea38e6a7c46a71c124d6
juraantoniv <91378718+juraantoniv@users.noreply.github.com>
2024-02-07 21:10:34 +0100

    Delete dist directory
    delete

--- dist/enums/gender.enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EGenders = void 0;
-var EGenders;
-(function (EGenders) {
-    EGenders["Male"] = "male";
-    EGenders["Female"] = "female";
-    EGenders["Other"] = "other";
-})(EGenders || (exports.EGenders = EGenders = {}));
